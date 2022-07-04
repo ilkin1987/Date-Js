b36ca7240e1356a264edbecb6ff800acb6daa4b7
ilkin1987 <101628203+ilkin1987@users.noreply.github.com>
2022-07-04 21:46:40 +0400

    concat-js

---
+++ Concat/main.js
@@ -0,0 +1,7 @@
+var animal=["it","pisik","canavar","peleng"];
+var people=["Azer","Memmed","Sexavet","Ramiz"];
+var cars=["Audi","Mercedes","Nissan","Chevrolet"];
+
+var plus=people.concat(animal,cars);
+
+document.write(plus)
